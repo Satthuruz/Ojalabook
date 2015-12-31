1c28c982d56a207bf906d2a4c806eb5ff02815b6
Stalin <satthuruz@gmail.com>
2015-12-31 05:31:00 -0500

    Creado el proyecto con status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ojalabook::Application.config.secret_token = 'ff02ecc963f6d3244e84bad1cd44d31317b87c6eb053ab5ccdf09edfdaf7be99f18027a477fe55043f13315db2bbf6b47e883106b21d82db15025bc008326602'
